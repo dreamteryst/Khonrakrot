bd9d8f3ece0ca220cb1dc5cd0140f50e9ac1a083
dreamteryst <dreamt451@email.com>
2018-02-25 16:19:27 +0700

    Start Project

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,21 @@ import { NgModule } from '@angular/core';
 
 
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
+import { MainComponent } from './main/main.component';
+import { TopNavComponent } from './top-nav/top-nav.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    HeaderComponent,
+    FooterComponent,
+    MainComponent,
+    TopNavComponent
   ],
   imports: [
     BrowserModule
