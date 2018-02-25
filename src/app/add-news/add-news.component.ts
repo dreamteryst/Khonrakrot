6d754faf0d72875d53211d3d68540a3340d138f0
dreamteryst <dreamt451@email.com>
2018-02-25 21:53:42 +0700

    success design

---
+++ src/app/add-news/add-news.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-add-news',
+  templateUrl: './add-news.component.html',
+  styleUrls: ['./add-news.component.css']
+})
+export class AddNewsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+    window.dispatchEvent(new CustomEvent('initSummernote'));
+  }
+
+}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
 
 
 import { AppComponent } from './app.component';
@@ -9,6 +10,24 @@ import { FooterComponent } from './footer/footer.component';
 import { MainComponent } from './main/main.component';
 import { TopNavComponent } from './top-nav/top-nav.component';
 import { NewsComponent } from './news/news.component';
+import { AddNewsComponent } from './add-news/add-news.component';
+import { ErrorComponent } from './error/error.component';
+
+const appRoutes: Routes = [
+  { 
+    path: '', 
+    component: MainComponent,
+    children: [
+      { path: '', component: NewsComponent },
+      { path: 'home', component: NewsComponent },
+      { path: 'addnews', component: AddNewsComponent }
+    ]
+  },
+  { path: 'home',
+    component: MainComponent
+  },
+  { path: '**', component: ErrorComponent }
+];
 
 
 @NgModule({
@@ -19,9 +38,15 @@ import { NewsComponent } from './news/news.component';
     FooterComponent,
     MainComponent,
     TopNavComponent,
-    NewsComponent
+    NewsComponent,
+    AddNewsComponent,
+    ErrorComponent
   ],
   imports: [
+    RouterModule.forRoot(
+      appRoutes,
+      { enableTracing: true } // <-- debugging purposes only
+    ),
     BrowserModule
   ],
   providers: [],
