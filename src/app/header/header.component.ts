25091c921b9cf4b2fcfdf82ae0f9afb00c5438ea
dreamteryst <dreamt451@email.com>
2018-02-27 15:18:51 +0700

    news cover success

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
+import { Observable } from '@firebase/util';
 
 @Component({
   selector: 'app-header',
@@ -6,6 +7,8 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./header.component.css']
 })
 export class HeaderComponent implements OnInit {
+
+  @Input() items: Observable<Object>;
 
   constructor() { }
 
