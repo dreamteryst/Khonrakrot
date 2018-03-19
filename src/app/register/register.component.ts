d0293c98a660d9addd98c1aff87f21f0a25f729f
dreamteryst <dreamt451@email.com>
2018-03-19 18:33:02 +0700

    register form

--- src/app/register/register.component.ts
+++ src/app/register/register.component.ts
@@ -7,7 +7,20 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RegisterComponent implements OnInit {
 
-  constructor() { }
+  questions = [
+    {'label':'รถคันแรกของคุณ', 'value': 'รถคันแรกของคุณ'}, 
+    {'label':'รถปัจจุบันของคุณ', 'value': 'รถปัจจุบันของคุณ'}, 
+    {'label':'เพลงโปรดของคุณ', 'value': 'เพลงโปรดของคุณ'}, 
+    {'label':'ชื่อโรงเรียนมัธยม', 'value': 'ชื่อโรงเรียนมัธยม'}
+  ];
+  question1:String;
+  question2:String;
+  question3:String;
+  answer1:String;
+  answer2:String;
+  answer3:String;
+
+  constructor() {}
 
   ngOnInit() {
   }
