261568ab73914ada507725688117a3a3f720bdd5
dreamteryst <dreamt451@gmail.com>
2018-03-27 22:10:24 +0700

    fetch data form database

--- src/app/show-news/show-news.component.ts
+++ src/app/show-news/show-news.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { Observable } from 'rxjs/Observable';
+import { map } from 'rxjs/operators';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-show-news',
@@ -6,10 +10,35 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./show-news.component.css']
 })
 export class ShowNewsComponent implements OnInit {
+  private id:number;
+  private item:any;
+  private topic:string;
+  private date_post:string;
+  private content:string;
 
-  constructor() { }
+  constructor(private route: ActivatedRoute, private http: HttpClient) {}
 
   ngOnInit() {
+    this.route.params.subscribe( params => {
+      this.id = params.id;
+    });
+    // this.items = this.http.get(window['domain'] + '/api/news/read.php?id='+this.id)
+    // .share()
+    // .pipe(
+    //   map(res => res['message']) // or any other operator
+    // );
+    this.http.get(window['domain'] + '/api/news/read.php?id='+this.id)
+    .subscribe((res) => {
+      this.item = res['message'][0];
+      this.topic = this.item['topic'];
+      this.content = this.item['content'];
+      this.date_post = this.item['date_post'];
+    });
+  }
+
+  nl2br (str, is_xhtml) {   
+      var breakTag = (is_xhtml || typeof is_xhtml === 'undefined') ? '<br />' : '<br>';    
+      return (str + '').replace(/([^>\r\n]?)(\r\n|\n\r|\r|\n)/g, '$1'+ breakTag +'$2');
   }
 
 }
