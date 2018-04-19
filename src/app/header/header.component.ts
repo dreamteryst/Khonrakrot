a60466c667fa5c8c256fab6b52a5d54f778b4ffa
dreamteryst <dreamt451@gmail.com>
2018-04-19 13:38:37 +0700

    fix load item slowly

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,18 +1,23 @@
 import { Component, OnInit, Input } from '@angular/core';
-import { Observable } from '@firebase/util';
-
+import { Observable } from 'rxjs/Observable';
+import { HttpClient } from '@angular/common/http';
+import { map } from 'rxjs/operators';
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.css']
 })
 export class HeaderComponent implements OnInit {
+  items: Observable<Object>;
 
-  @Input() items: Observable<Object>;
-
-  constructor() { }
+  constructor(private http: HttpClient) { }
 
   ngOnInit() {
+    this.items = this.http.get(window['domain'] + '/api/news/read.php?type=cover')
+        .share()
+        .pipe(
+          map(res => res['message']) // or any other operator
+        );
   }
 
 }
